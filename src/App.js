aaa1ee107139cc9bbc63c125f0da4deab216fb9a
Kirtisaumya <135496593+Kirtisaumya@users.noreply.github.com>
2024-03-10 10:04:00 +0545

    Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import AboutUs from "./Pages/AboutUs/AboutUs.jsx";
+import Blog from "./Pages//Blog/Blog.jsx";
+import ContactUs from "./Pages/ContactUs/ContactUs.jsx";
+import Product from "./Pages/Product/Product.jsx";
+
+import Navbars from "./Components/navbar.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <div className="App">
+        <Router>
+          <Navbars />
+          <div className="content">
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/Blog" element={<Blog />} />
+              <Route path="/Product" element={<Product />} />
+              <Route path="/ContactUs" element={<ContactUs />} />
+              <Route path="/AboutUs" element={<AboutUs />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </CartProvider>
   );
 }
 
